0cfd70017805b600178de6e3ae8fe8651bfe41f6
Virgi02i87167 <131634183+Virgi02i87167@users.noreply.github.com>
2026-06-02 23:33:56 -0600

    fix

--- resources/views/reservaciones/index.blade.php
+++ resources/views/reservaciones/index.blade.php
@@ -55,13 +55,6 @@
                     </span>
                 </div>
 
-                <!-- Heart Icon (Decoration) -->
-                <div class="absolute top-4 right-4 text-white/90 hover:text-red-500 transition-colors drop-shadow-lg z-10">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                    </svg>
-                </div>
-
                 <!-- Controles del Carrusel (Flechas en Hover) -->
                 <template x-if="images.length > 1">
                     <div>
